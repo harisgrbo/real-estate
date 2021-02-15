2fcc696805d5874f2720b77866a9e275041a84b9
Haris Grbo <haris@misijaweb.com>
2021-02-15 19:27:45 +0100

    merge

--- components/includes/MobileBottomNavbar.vue
+++ components/includes/MobileBottomNavbar.vue
@@ -1,16 +1,89 @@
 <template>
   <div class="mobile-bottom-navbar">
-      <font-awesome-icon icon="th-large"></font-awesome-icon>
-      <font-awesome-icon icon="envelope"></font-awesome-icon>
-      <font-awesome-icon icon="plus"></font-awesome-icon>
-      <font-awesome-icon icon="user"></font-awesome-icon>
-      <font-awesome-icon icon="bars"></font-awesome-icon>
+<!--      <font-awesome-icon v-for="button in buttons" :icon="button.icon" @action="button.action"></font-awesome-icon>-->
+    <button @click="openSidebarMenu">test</button>
+      <client-only>
+<!--        <modal name="type" :adaptive="true" height="100%">-->
+<!--          <ListingType @selected-type="handleSelectedType" @close="$modal.hide('type')"></ListingType>-->
+<!--        </modal>-->
+        <modal name="sidebar" :adaptive="true" height="100%">
+          sidebar
+        </modal>
+      </client-only>
   </div>
 </template>
 b
 <script>
-export default {
-name: "MobileBottomNavbar"
+import { Component, Vue} from "nuxt-property-decorator";
+import CategoriesList from "@/components/CategoriesList";
+import ListingType from "@/components/ListingType";
+
+@Component({
+  components: {
+    ListingType,
+    CategoriesList
+  }
+})
+
+export default class MobileBottomNavbar extends Vue {
+  /*buttons = [
+    {
+      icon: 'th-large',
+      // action: this.openCategoriesModal()
+    },
+    {
+      icon: 'envelope',
+      action: this.$router.push('/poruke')
+    },
+    {
+      icon: 'plus',
+      action: this.$router.push('/objava')
+    },
+    {
+      icon: 'user',
+      action: this.$router.push('/users/' + this.$auth.user.id)
+    },
+    {
+      icon: 'bars',
+      // action: this.openSidebarMenu()
+    },
+  ]*/
+
+  openSidebarMenu() {
+    this.$modal.show('sidebar')
+  }
+
+  // openCategoriesModal() {
+  //   let self = this;
+  //   this.$modal.show(
+  //     {
+  //       render(h) {
+  //
+  //         return h( CategoriesList, {
+  //           props: {
+  //           },
+  //           on: {
+  //             selectedCat(e) {
+  //               self.selectedCategory = e;
+  //               self.closeModal();
+  //             },
+  //             close() {
+  //               // close event needs to be echoed
+  //               self.closeModal();
+  //             }
+  //           }
+  //         });
+  //       }
+  //     },
+  //     {
+  //     },
+  //     {
+  //       'selected-category': this.handleSelectedCategory,
+  //       adaptive: true,
+  //       height: "100%",
+  //     }
+  //   );
+  // }
 }
 </script>
 
