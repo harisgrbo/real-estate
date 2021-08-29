4abd881004e2997c228c2db17576f12fd0649c1d
Haris Grbo <haris@misijaweb.com>
2021-08-29 16:17:06 +0200

    merge

--- pages/publish.vue
+++ pages/publish.vue
@@ -1,39 +1,5 @@
 <template>
   <div class="publish-wrapper-inner">
-      <div v-if="!$device.isMobile" class="progress-wrapper mt-6">
-        <client-only>
-          <radial-progress-bar :diameter="200"
-                               :animateSpeed="300"
-                               :completed-steps="completion"
-                               :total-steps="100"
-                               startColor="#0B8489"
-                               endColor="#1B1E31"
-                               :strokeWidth="10"
-                               :innerStrokeWidth="10"
-                               innerStrokeColor="#f1f1f1"
-          >
-            <p class="centered">{{ completion.toFixed() }} %</p>
-          </radial-progress-bar>
-        </client-only>
-        <div class="radial-steps">
-          <div class="rounded-md bg-blue-50 p-4 mb-6 mt-6">
-            <div class="flex">
-              <div class="flex-shrink-0">
-                <!-- Heroicon name: solid/information-circle -->
-                <svg class="h-5 w-5 text-blue-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" />
-                </svg>
-              </div>
-              <div class="ml-3 flex-1 md:flex md:justify-between">
-                <p class="text-sm text-blue-700">
-                  Unošenjem što više informacija o Vašem oglasu, omogućujete da on bude vidljiviji većem broju korisnika. Oglasi sa preko 80% popunjenih informacija ulaze u listu najpregledavanijih na sajtu.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
       <div class="content-wrapper my-6">
         <div v-show="currentStep === steps.STEP_ONE" class="step-1">
           <h1 class="heading">
