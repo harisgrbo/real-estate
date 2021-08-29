949fd73dfb47f2831f46403c76303f2b75087b1c
Haris Grbo <haris@misijaweb.com>
2021-08-29 16:17:38 +0200

    merge

--- pages/index.vue
+++ pages/index.vue
@@ -63,7 +63,25 @@
           </div>
           <div>
             <label>Kategorija</label>
-            <TextField type="text" placeholder="Izaberite kategoriju npr. stan"></TextField>
+<!--            <TextField type="text" placeholder="Izaberite kategoriju npr. stan"></TextField>-->
+            <fieldset class="w-full">
+              <label for="language" class="sr-only">Language</label>
+              <div class="relative">
+                <select id="language" name="language" class="appearance-none block w-full bg-none bg-white rounded-md py-2 h-12 text-base text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <option selected>English</option>
+                  <option>French</option>
+                  <option>German</option>
+                  <option>Japanese</option>
+                  <option>Spanish</option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
+                  <!-- Heroicon name: solid/chevron-down -->
+                  <svg class="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  </svg>
+                </div>
+              </div>
+            </fieldset>
           </div>
           <div>
             <label>Cijena</label>
@@ -86,7 +104,26 @@
           </div>
           <div>
             <label>Kategorija</label>
-            <TextField type="text" placeholder="Izaberite kategoriju npr. stan"></TextField>
+<!--            <TextField type="text" placeholder="Izaberite kategoriju npr. stan"></TextField>-->
+            <fieldset class="w-full">
+              <label for="language" class="sr-only">Language</label>
+              <div class="relative">
+                <select id="language" name="language" class="appearance-none block w-full bg-none bg-white rounded-md py-2 h-12 text-base text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                  <option selected>English</option>
+                  <option>French</option>
+                  <option>German</option>
+                  <option>Japanese</option>
+                  <option>Spanish</option>
+                </select>
+                <div class="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
+                  <!-- Heroicon name: solid/chevron-down -->
+                  <svg class="h-4 w-4 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  </svg>
+                </div>
+              </div>
+            </fieldset>
+
           </div>
           <div>
             <label>Cijena</label>
