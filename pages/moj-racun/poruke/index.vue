d5333c5dea06a676458a4df11664b76622ea7a13
Admir Ferhatovic <admir@mukur.com>
2021-12-27 13:47:46 +0100

    fix

--- pages/moj-racun/poruke/index.vue
+++ pages/moj-racun/poruke/index.vue
@@ -17,7 +17,7 @@
                       <div class="text-xs text-gray-500 ml-auto">{{ $moment(conversation.last_message.created_at).format("DD.MM.YYYY") }}</div>
                     </div>
                     <div class="w-full truncate text-gray-600 mt-0.5 flex flex-row items-center justify-between">
-                      <div class="w-full flex items-center flex-row truncate text-sm text-gray-900 mt-0.5" v-if="conversation.last_message.content.mime.substr(0, 5) === 'image'">
+                      <div class="w-full flex items-center flex-row truncate text-sm text-gray-900 mt-0.5" v-if="conversation.last_message.message_type === 'media' && conversation.last_message.content.mime.substr(0, 5) === 'image'">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" @click="$modal.show('send-image')">
                           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                         </svg> Korisnik je poslao sliku
