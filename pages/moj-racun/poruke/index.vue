70f1c885193f69cea7c9596ae7cb7b6257ea51b0
Haris Grbo <haris@misijaweb.com>
2022-02-13 20:55:46 +0100

    merge

--- pages/moj-racun/poruke/index.vue
+++ pages/moj-racun/poruke/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="preview-wrapper-inner">
+  <div class="preview-wrapper-inner relative">
     <!-- BEGIN: Content -->
     <div class="flex w-full">
       <div v-if="conversations.length > 0" class="chat flex flex-row w-full">
@@ -184,7 +184,77 @@
       </div>
       <NotFound v-else src="/no-messages.svg" text="Nemate poruka"></NotFound>
     </div>
-    <modal name="conversations" @before-open="beforeOpen" @before-close="beforeClose" :adaptive="true" height="100%">
+    <client-only>
+      <modal @before-open="beforeOpen" @before-close="beforeClose" name="send-image" :adaptive="true" height="100%">
+        <div class="modal-inner">
+          <div class="modal-header">
+            <h2>Slanje slike</h2>
+            <i class="material-icons" @click.prevent="$modal.hide('send-image')">close</i>
+          </div>
+          <div class="modal-content places-modal pt-4">
+              <label for="upload" class="image-upload-wrapper">
+                <input
+                  id="upload"
+                  type="file"
+                  @change="preview"
+                  class="hidden"
+                />
+                <img v-if="imgSrc.length" id="image" :src="imgSrc" class="mt-md" />
+                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="#1F2937">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+              </label>
+              <div class="flex flex-row items-center justify-between mt-md">
+                <ActionButton
+                  class="w-full mr-2"
+                  placeholder="Pošalji"
+                  :style-options="{width: '100%'}"
+                  @action="upload(imgBlob); showImageUpload = false;"
+                />
+                <ActionButton
+                  class="w-full ml-2"
+                  :style-options="{width: '100%'}"
+                  placeholder="Odustani"
+                  @action="showImagePreviewModal = false; imgSrc = ''; $modal.hide('send-image') "
+                />
+              </div>
+          </div>
+        </div>
+      </modal>
+    </client-only>
+    <client-only>
+      <modal @before-open="beforeOpen" @before-close="beforeClose" name="delete-conversation" :adaptive="true" height="100%">
+        <div class="modal-inner">
+          <div class="modal-header">
+            <h2>Brisanje razgovora</h2>
+            <i class="material-icons" @click.prevent="$modal.hide('delete-conversation')">close</i>
+          </div>
+          <div class="modal-content places-modal pt-4">
+            <div class="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-4 mt-4">
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <!-- Heroicon name: solid/exclamation -->
+                  <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+                  </svg>
+                </div>
+                <div class="ml-3">
+                  <p class="text-sm text-yellow-700">
+                    Da li ste sigurni da želite izbrisati razgovor
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-row items-center justify-between">
+              <action-button :style-options="{ marginRight: '8px', width: '100%' }" @action="deleteConversation(currentConversation); $modal.hide('delete-conversation')" placeholder="Da"></action-button>
+              <action-button :style-options="{ marginLeft: '8px', width: '100%' }" placeholder="Ne" @action="$modal.hide('delete-conversation')"></action-button>
+            </div>
+          </div>
+        </div>
+      </modal>
+    </client-only>
+    <div class="conversation-div" v-if="showConversationDiv">
       <div class="modal-inner">
         <div class="modal-content">
           <div class="col-span-12 lg:col-span-8 2xl:col-span-9">
@@ -211,7 +281,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                       </svg>
                     </div>
-                    <i class="material-icons ml-4" @click="$modal.hide('conversations')">close</i>
+                    <i class="material-icons ml-4" @click="showConversationDiv = false">close</i>
                   </div>
                 </div>
                 <div v-show="messagesLoaded" ref="messageContainer" class="overflow-y-scroll scrollbar-hidden mobile-height pt-5 flex-1">
@@ -305,77 +375,8 @@
           </div>
         </div>
       </div>
-    </modal>
-    <client-only>
-      <modal @before-open="beforeOpen" @before-close="beforeClose" name="send-image" :adaptive="true" height="100%">
-        <div class="modal-inner">
-          <div class="modal-header">
-            <h2>Slanje slike</h2>
-            <i class="material-icons" @click.prevent="$modal.hide('send-image')">close</i>
-          </div>
-          <div class="modal-content places-modal pt-4">
-              <label for="upload" class="image-upload-wrapper">
-                <input
-                  id="upload"
-                  type="file"
-                  @change="preview"
-                  class="hidden"
-                />
-                <img v-if="imgSrc.length" id="image" :src="imgSrc" class="mt-md" />
-                <svg v-else xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="#1F2937">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z" />
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-              </label>
-              <div class="flex flex-row items-center justify-between mt-md">
-                <ActionButton
-                  class="w-full mr-2"
-                  placeholder="Pošalji"
-                  :style-options="{width: '100%'}"
-                  @action="upload(imgBlob); showImageUpload = false;"
-                />
-                <ActionButton
-                  class="w-full ml-2"
-                  :style-options="{width: '100%'}"
-                  placeholder="Odustani"
-                  @action="showImagePreviewModal = false; imgSrc = ''; $modal.hide('send-image') "
-                />
-              </div>
-          </div>
-        </div>
-      </modal>
-    </client-only>
-    <client-only>
-      <modal @before-open="beforeOpen" @before-close="beforeClose" name="delete-conversation" :adaptive="true" height="100%">
-        <div class="modal-inner">
-          <div class="modal-header">
-            <h2>Brisanje razgovora</h2>
-            <i class="material-icons" @click.prevent="$modal.hide('delete-conversation')">close</i>
-          </div>
-          <div class="modal-content places-modal pt-4">
-            <div class="bg-yellow-50 border-l-4 border-yellow-400 p-4 mb-4 mt-4">
-              <div class="flex">
-                <div class="flex-shrink-0">
-                  <!-- Heroicon name: solid/exclamation -->
-                  <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-                  </svg>
-                </div>
-                <div class="ml-3">
-                  <p class="text-sm text-yellow-700">
-                    Da li ste sigurni da želite izbrisati razgovor
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="flex flex-row items-center justify-between">
-              <action-button :style-options="{ marginRight: '8px', width: '100%' }" @action="deleteConversation(currentConversation); $modal.hide('delete-conversation')" placeholder="Da"></action-button>
-              <action-button :style-options="{ marginLeft: '8px', width: '100%' }" placeholder="Ne" @action="$modal.hide('delete-conversation')"></action-button>
-            </div>
-          </div>
-        </div>
-      </modal>
-    </client-only>
+    </div>
+
     <div v-if="openImageGallery" class="image-gallery" @click="openImageGallery = false; selectedImage = ''">
       <div class="img-wrap">
         <img src="/svg/close.svg" alt="" class="close">
@@ -420,6 +421,7 @@ import SmallListingCard from "../../../components/SmallListingCard";
 
 export default class Poruke extends Vue {
   messages = [];
+  showConversationDiv = false;
   messagesLoaded = true;
   currentConversation = null;
   messageContent = '';
@@ -658,7 +660,7 @@ export default class Poruke extends Vue {
       await this.fetchMessages(conv.id);
 
       this.scrollBottom();
-      this.$modal.show('conversations')
+      this.showConversationDiv = true;
     }
   }
 
@@ -1175,6 +1177,16 @@ img {
 .add-border {
   border: 1px solid #f1f1f1;
   background-color: #fff;
+}
+
+.conversation-div {
+  background: #fff;
+  position: fixed;
+  z-index: 9999;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
 }
 </style>
 
