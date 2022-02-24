796d566f9e1e7b00487bb557a0c83d3fc9b4fd28
Haris Grbo <haris@misijaweb.com>
2022-02-24 10:48:31 +0100

    merge

--- components/listingCard/ListingCard.vue
+++ components/listingCard/ListingCard.vue
@@ -67,7 +67,7 @@
                 {{ listing.title }}
               </h2>
             </div>
-            <div class="icons-date flex flex-row items-center justify-between w-full" v-if="listing.price !== 0">
+            <div class="icons-date flex flex-row items-center justify-between w-full" v-if="listing.price != 0">
               <div class="important">
                 <p :class="['new', listing.hasOwnProperty('discount') ? 'cross' : '']">{{ parseInt(listing.price).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".") }} KM</p>
                 <p v-show="listing.is_booking && !listing.hasOwnProperty('discount')" class="pl-2">/ noć {{ listing.per_guest ? 'po osobi' : '' }}</p>
