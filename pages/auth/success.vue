33944bbf060777fe642b257c631870062d4c9725
Admir Ferhatovic <admir.ferhatovic@olx.ba>
2022-06-15 22:48:13 +0200

    google auth skeleton

---
+++ pages/auth/success.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Uspjeh</div>
+</template>
+
+<script>
+
+import {Component, Vue} from "nuxt-property-decorator";
+
+@Component({})
+export default class Success extends Vue {
+  async created() {
+    let token = this.$route.query.token;
+
+    if (token) {
+      await this.$auth.setUserToken(token);
+    }
+
+    await this.$router.push('/');
+  }
+}
+
+</script>
